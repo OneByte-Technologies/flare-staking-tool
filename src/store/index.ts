c98e9d9c4059c5c778545ba1310fad6d576cb5d3
Kunal Jha <106550721+Kun-J@users.noreply.github.com>
2023-10-02 14:58:39 +0530

    delegate-changes (#49)
    * pchain-fix
    
    * delegator-changes
    
    * keystore-name-change

--- src/store/index.ts
+++ src/store/index.ts
@@ -294,7 +294,7 @@ export default new Vuex.Store({
 
                 const utcDate = new Date()
                 const dateString = utcDate.toISOString().replace(' ', '_')
-                const filename = `AVAX_${dateString}.json`
+                const filename = `FLR_${dateString}.json`
 
                 const blob = new Blob([text], {
                     type: 'application/json',
